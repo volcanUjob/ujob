aa952973fae5c9741d24c9cccc280350be4a09f9
marwen <marwenkhorchani7@gmail.com>
2021-01-05 10:11:14 +0100

    map fixed

--- ujob/src/app/map/map.component.ts
+++ ujob/src/app/map/map.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { MapsService } from "./maps.service";
 
-@Component({
+@Component( {
   selector: "app-map",
   templateUrl: "/src/app/map/map.component.html",
   styleUrls: ["/src/app/map/map.component.scss"],
-})
+} )
 export class MapComponent {
   lat = 36.9922751;
   lng = 10.1255164;
@@ -15,19 +15,19 @@ export class MapComponent {
   city = "";
   ip = 0;
   location: Object | undefined;
-
-  constructor(private map: MapsService) {}
+  constructor( private map: MapsService ) { }
+}
 
   // tslint:disable-next-line: use-life-cycle-interface
-  // ngOnInit() {
-  //   this.map.getLocation().subscribe((data) => {
-  //     console.log(data);
-  //     this.lat = data.latitude;
-  //     this.lng = data.longitude;
-  //     this.country = data.country_name;
-  //     this.calling_code = data.country_calling_code;
-  //     this.city = data.city;
-  //     this.ip = data.ip;
-  //   });
-  // }
-}
+//   ngOnInit() {
+//     this.map.getLocation().subscribe( ( data ) => {
+//       console.log( data );
+//       this.lat = data.latitude;
+//       this.lng = data.longitude;
+//       this.country = data.country_name;
+//       this.calling_code = data.country_calling_code;
+//       this.city = data.city;
+//       this.ip = data.ip;
+//     } );
+//   }
+// }
