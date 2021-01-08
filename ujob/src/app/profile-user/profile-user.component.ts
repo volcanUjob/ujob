e96b40b0852f0d99ee10c4552c0e64adf0e139a9
marwen <marwenkhorchani7@gmail.com>
2021-01-08 03:18:18 +0100

    template fixed

--- ujob/src/app/profile-user/profile-user.component.ts
+++ ujob/src/app/profile-user/profile-user.component.ts
@@ -7,25 +7,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./profile-user.component.scss']
 })
 export class ProfileUserComponent implements OnInit {
-  isLoggedIn = false
+  isLoggedIn = false;
   data: any;
-  postCmp: any[] = [];
   allpost: any;
   posts: any = [];
   image: any = '';
-  constructor(private router: Router,private httpClient: HttpClient) { }
+  constructor(private router: Router) {}
   currentFriend(friend: any) {
     localStorage.setItem('friend', JSON.stringify(friend));
   }
   ngOnInit(): void {
     this.data = JSON.parse(localStorage.getItem('friend') || 'hello');
+
     this.allpost = JSON.parse(localStorage.getItem('allposts') || 'hello');
+
     var id = this.data._id;
+
     for (var i = 0; i < this.allpost.length; i++) {
       if (this.allpost[i].posterId._id === id) {
         this.posts.push(this.allpost[i]);
       }
     }
+    console.log('all', this.posts);
+
+    console.log('newposts', this.posts);
   }
   onlogout() {
     this.isLoggedIn = false;
