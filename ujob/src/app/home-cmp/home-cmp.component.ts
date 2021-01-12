35c833848c81becba87bbf1690bbdca8b28d331c
RafrafiAhmed <ahmedrafrafi25@gmail.com>
2021-01-12 15:09:23 +0100

    comment done

--- ujob/src/app/home-cmp/home-cmp.component.ts
+++ ujob/src/app/home-cmp/home-cmp.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
 @Component({
   selector: 'app-home-cmp',
   templateUrl: './home-cmp.component.html',
   styleUrls: ['./home-cmp.component.scss'],
 })
 export class HomeCmpComponent implements OnInit {
+  commentPostCmp:any[]=[]
+  comment: any;
+  showModal: boolean = true;
   postCmp: any[] = [];
   filter: any = [];
   name = '';
   image: any = '';
   pathOrigine: string = 'http://localhost:3000/';
-  constructor(private httpClient: HttpClient) {}
+  user = JSON.parse(localStorage.getItem('user') || '{}');
+
+  constructor(private httpClient: HttpClient,  @Inject(DOCUMENT) document) {}
 
   currentFriend(friend: any) {
     localStorage.setItem('friend', JSON.stringify(friend));
   }
 
   ngOnInit(): void {
+   
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -40,6 +48,57 @@ export class HomeCmpComponent implements OnInit {
           this.filter[i].posterId.image = this.image;
         }
       });
+  }
+  closeWindow() {
+    var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='none';
+        this.commentPostCmp=[]
+   
+  }
+  showComments(id: any) {
+
+     const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    this.httpClient
+    .get(this.pathOrigine+'comments/'+id,httpOptions)
+    .subscribe((comments:any)=>{
+        this.commentPostCmp.push(...comments)
+        var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='block';
+    this.showModal = true;
+    })
+    
+    var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='block';
+    this.showModal = true;
+  }
+
+  postComment(id: any) {
+    if(document.getElementById('elem'+id)!== null){
+    this.comment=document.getElementById('elem'+ id).value;
+    var obj = {
+      comment: this.comment,
+      postId: id,
+      commenterId: this.user._id,
+    };
+    console.log(obj);
+    this.httpClient
+      .post(this.pathOrigine + 'comment', obj)
+      .subscribe((res: any) => {
+        console.log(res);
+        alert('comment created');
+        this.comment = '';
+      });
+    }else{
+      alert('id undefined');
+    }
   }
   numberOfLikes: number = 0;
 
--- ujob/src/app/home-user/home-user.component.ts
+++ ujob/src/app/home-user/home-user.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-home-user',
   templateUrl: './home-user.component.html',
   styleUrls: ['./home-user.component.scss'],
 })
 export class HomeUserComponent implements OnInit {
+  commentPostCmp:any[]=[]
+  comment: any;
+  showModal: boolean = true;
   postCmp: any[] = [];
   filter: any = [];
   name = '';
   image: any = '';
-  x = {};
-  imgpostcmp: any = [];
   pathOrigine: string = 'http://localhost:3000/';
-  constructor(private httpClient: HttpClient) {}
+  user = JSON.parse(localStorage.getItem('user') || '{}');
+
+  constructor(private httpClient: HttpClient,  @Inject(DOCUMENT) document) {}
 
   currentFriend(friend: any) {
     localStorage.setItem('friend', JSON.stringify(friend));
   }
   ngOnInit(): void {
+   
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -30,9 +35,7 @@ export class HomeUserComponent implements OnInit {
       .subscribe((data: any) => {
         this.postCmp.push(...data);
         localStorage.setItem('allposts', JSON.stringify(this.postCmp));
-
         this.filter.push(...data);
-
         for (var i = 0; i < this.filter.length; i++) {
           var x = this.filter[i].posterId.image.toString().split('');
           var img = '';
@@ -44,7 +47,57 @@ export class HomeUserComponent implements OnInit {
         }
       });
   }
+  closeWindow() {
+    var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='none';
+        this.commentPostCmp=[]
+   
+  }
+  showComments(id: any) {
 
+     const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    this.httpClient
+    .get(this.pathOrigine+'comments/'+id,httpOptions)
+    .subscribe((comments:any)=>{
+        this.commentPostCmp.push(...comments)
+        var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='block';
+    this.showModal = true;
+    })
+    
+    var modal = document.getElementById('myModal');
+    if(modal!==null)
+        modal.style.display='block';
+    this.showModal = true;
+  }
+
+  postComment(id: any) {
+    if(document.getElementById('elem'+id)!== null){
+    this.comment=document.getElementById('elem'+ id).value;
+    var obj = {
+      comment: this.comment,
+      postId: id,
+      commenterId: this.user._id,
+    };
+    console.log(obj);
+    this.httpClient
+      .post(this.pathOrigine + 'comment', obj)
+      .subscribe((res: any) => {
+        console.log(res);
+        alert('comment created');
+        this.comment = '';
+      });
+    }else{
+      alert('id undefined');
+    }
+  }
   numberOfLikes: number = 0;
 
   likeButtonClick() {
@@ -56,7 +109,6 @@ export class HomeUserComponent implements OnInit {
     this.numberOfLikes--;
     console.log(this.numberOfLikes);
   }
-
   onChange(event: any) {
     this.filter = this.postCmp.filter((item: any) => {
       if (
